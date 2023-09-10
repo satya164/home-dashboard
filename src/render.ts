4ea0174d2ec569adee6c36d7b0103c5cb7a148a4
Satyajit Sahoo <satyajit.happy@gmail.com>
2023-09-10 15:20:07 +0200

    fix: set initial scale for mobile

--- src/render.ts
+++ src/render.ts
@@ -8,6 +8,7 @@ export function render({ apps, dashdot }: Config) {
     <html lang="en">
       <head>
         <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Dashboard</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
