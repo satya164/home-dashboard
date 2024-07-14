edf6376f9f94bc259412d20ec581ee3248630c40
Satyajit Sahoo <satyajit.happy@gmail.com>
2024-07-14 14:55:30 +0200

    feat: add colors for high system usage

--- public/index.js
+++ public/index.js
@@ -1,20 +1,46 @@
 async function fetchSystemInfo() {
   const system = await fetch('/api/system-info').then((res) => res.json());
 
+  const cpu = document.getElementById('cpu');
+  const ram = document.getElementById('ram');
+  const storage = document.getElementById('storage');
+
   if (system.cpu) {
-    document.getElementById('cpu').textContent = `${system.cpu.toFixed(2)}%`;
+    cpu.textContent = `${system.cpu.toFixed(2)}%`;
+  }
+
+  if (system.cpu >= 70) {
+    cpu.classList.add('warning');
+  } else if (system.cpu >= 90) {
+    cpu.classList.add('danger');
+  } else {
+    cpu.classList.remove('warning', 'danger');
   }
 
   if (system.ram) {
-    document.getElementById('ram').textContent = `${system.ram.toFixed(2)}%`;
+    ram.textContent = `${system.ram.toFixed(2)}%`;
+  }
+
+  if (system.ram >= 70) {
+    ram.classList.add('warning');
+  } else if (system.ram >= 90) {
+    ram.classList.add('danger');
+  } else {
+    ram.classList.remove('warning', 'danger');
   }
 
   if (system.storage) {
-    document.getElementById(
-      'storage'
-    ).textContent = `${system.storage.used.toFixed(
+    storage.textContent = `${system.storage.used.toFixed(
       2
     )} GB / ${system.storage.total.toFixed(2)} GB`;
+  }
+
+  if (system.storage.used >= system.storage.total * 0.7) {
+    storage.classList.add('warning');
+  } else if (system.storage.used >= system.storage.total * 0.9) {
+    storage.classList.add('danger');
+  } else {
+    storage.classList.remove('warning', 'danger');
   }
 
   document.getElementById('system-info').dataset.visible = 'true';
